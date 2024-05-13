d1fd226441605b4ac77c8dfe48d5260cd585ef49
Tranfersafety <ratthaphumi.safe@gmail.com>
2024-05-13 17:03:02 +0700

    first commit

---
+++ switch.js
@@ -0,0 +1,52 @@
+const dayNumber = 5;
+switch (dayNumber) {
+  case 0:
+    console.log("sunday");
+    break;
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+    break;
+  case 4:
+    console.log("thursday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+    console.log("saturday");
+    break;
+  default:
+    console.log("Invalid day number");
+}
+
+switch (true) {
+  case dayNumber === 0:
+    console.log("sunday");
+    break;
+  case dayNumber === 1:
+    console.log("monday");
+    break;
+  case dayNumber === 2:
+    console.log("tuesday");
+    break;
+  case dayNumber === 3:
+    console.log("wednesday");
+    break;
+  case dayNumber === 4:
+    console.log("thursday");
+    break;
+  case dayNumber === 5:
+    console.log("friday");
+    break;
+  case dayNumber === 6:
+    console.log("saturday");
+    break;
+  default:
+    console.log("Invalid day number");
+}
